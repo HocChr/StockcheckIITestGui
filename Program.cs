d884c3ecfb31137563f3d817513548c788b5cb50
Hochheim Christian <chochheim@pb-group.de>
2020-05-28 15:01:41 +0200

    Design fertig

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace StockcheckIITestGui
+{
+    public class TestGui : IGui
+    {
+        public void SetStocks(List<StockCheckerII.StockEntity> stocks)
+        {
+
+        }
+    }
+}
