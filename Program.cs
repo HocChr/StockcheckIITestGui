025ed73f73af655686fa4f94a8b49a6474a55feb
Hochheim Christian <chochheim@pb-group.de>
2020-06-03 13:59:49 +0200

    Stockchecker auswertung fertig; mit Consolen GUI

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System;
 
 namespace StockcheckIITestGui
 {
@@ -7,6 +8,28 @@ namespace StockcheckIITestGui
         public void SetStocks(List<StockCheckerII.StockEntity> stocks)
         {
 
+            // Display header
+            var header = String.Format("{0,-20}{1,8}{2,20}{3,18}{4,16}{5,20}{6,20}\n",
+                                          "Name",
+                                           "Jahr",
+                                            "Gewinnkorrelation",
+                                             "Dividenwachstum",
+                                             "Gewinnwachstum",
+                                             "Ausschüttungsquote",
+                                             "Bemerkung");
+            Console.WriteLine(header);
+            foreach (StockCheckerII.StockEntity stock in stocks)
+            {
+                var output = String.Format("{0,-20}{1,8}{2,20}{3,18}{4,16}{5,20}{6,45}",
+                       stock.Name.Substring(0, Math.Min(stock.Name.Length, 20)),
+                        stock.GetYearData()[stock.GetYearData().Count - 1].Year,
+                         stock.EarningCorrelation,
+                          stock.DividendGrowthFiveYears,
+                          stock.EarningGrowthTreeYears,
+                          stock.PayoutRatio,
+                          stock.GetRemarks().Substring(0, Math.Min(stock.GetRemarks().Length, 45)));
+                Console.WriteLine(output);
+            }
         }
     }
 }
