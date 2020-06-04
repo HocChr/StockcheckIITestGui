03de3e518ac5164f3db3619479ad97697f99160a
Hochheim Christian <chochheim@pb-group.de>
2020-06-04 15:31:34 +0200

    Berechnung steht

--- Program.cs
+++ Program.cs
@@ -7,29 +7,41 @@ namespace StockcheckIITestGui
     {
         public void SetStocks(List<StockCheckerII.StockEntity> stocks)
         {
-
             // Display header
-            var header = String.Format("{0,-20}{1,8}{2,20}{3,18}{4,16}{5,20}{6,20}\n",
-                                          "Name",
-                                           "Jahr",
-                                            "Gewinnkorrelation",
-                                             "Dividenwachstum",
-                                             "Gewinnwachstum",
-                                             "Ausschüttungsquote",
-                                             "Bemerkung");
+            var header = String.Format("{0,-20}{1,8}{2,7}{3,18}{4,16}{5,15}{6,19}\n",
+                                        "Name",
+                                        "Jahr",
+                                        "Rating",
+                                        "Gewinnkorrelation",
+                                        "Dividenwachstum",
+                                        "Gewinnwachstum",
+                                        "Ausschüttungsquote");
             Console.WriteLine(header);
             foreach (StockCheckerII.StockEntity stock in stocks)
             {
-                var output = String.Format("{0,-20}{1,8}{2,20}{3,18}{4,16}{5,20}{6,45}",
-                       stock.Name.Substring(0, Math.Min(stock.Name.Length, 20)),
-                        stock.GetYearData()[stock.GetYearData().Count - 1].Year,
-                         stock.EarningCorrelation,
-                          stock.DividendGrowthFiveYears,
-                          stock.EarningGrowthTreeYears,
-                          stock.PayoutRatio,
-                          stock.GetRemarks().Substring(0, Math.Min(stock.GetRemarks().Length, 45)));
+                var output = String.Format("{0,-20}{1,7}{2,8}{3,18}{4,16}{5,15}{6,19}",
+                    stock.Name.Substring(0, Math.Min(stock.Name.Length, 20)),
+                    stock.GetYearData()[stock.GetYearData().Count - 1].Year,
+                    RatingToString(stock.Rating),
+                    stock.EarningCorrelation,
+                    stock.DividendGrowthFiveYears,
+                    stock.EarningGrowthTreeYears,
+                    stock.PayoutRatio);
                 Console.WriteLine(output);
             }
+        }
+
+        private string RatingToString(StockCheckerII.StockEntity.Rate rate)
+        {
+            if (rate == StockCheckerII.StockEntity.Rate.KAUFEN)
+            {
+                return "A";
+            }
+            if (rate == StockCheckerII.StockEntity.Rate.HALTEN)
+            {
+                return "B";
+            }
+            return "C";
         }
     }
 }
